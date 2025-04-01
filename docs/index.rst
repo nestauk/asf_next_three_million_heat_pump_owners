8bbd4c4379ca59dcb321d4f609695de29679324d
eglucas <elysialucas@gmail.com>
2025-04-01 12:21:17 +0100

    Setup Nesta Data Science cookiecutter

---
+++ docs/index.rst
@@ -0,0 +1,19 @@
+.. toctree::
+   :hidden:
+   :maxdepth: 1
+
+   api/asf_next_three_million_heat_pump_owners
+   License <license>
+
+asf_next_three_million_heat_pump_owners
+===============================
+
+A repo to manage data cleaning and analysis related to the survey conducted by YouGov for the geodemographic segmentation project “Identifying the next 3 million heat pump owners”.
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
---
+++ setup.py
@@ -0,0 +1,27 @@
+"""asf_next_three_million_heat_pump_owners."""
+
+from pathlib import Path
+from setuptools import find_packages
+from setuptools import setup
+
+
+def read_lines(path):
+    """Read lines of `path`."""
+    with open(path) as f:
+        return f.read().splitlines()
+
+
+BASE_DIR = Path(__file__).parent
+
+
+setup(
+    name="asf_next_three_million_heat_pump_owners",
+    long_description=open(BASE_DIR / "README.md").read(),
+    install_requires=read_lines(BASE_DIR / "requirements.txt"),
+    extras_require={"dev": read_lines(BASE_DIR / "requirements_dev.txt")},
+    packages=find_packages(exclude=["docs"]),
+    version="0.1.0",
+    description="A repo to manage data cleaning and analysis related to the survey conducted by YouGov for the geodemographic segmentation project “Identifying the next 3 million heat pump owners”.",
+    author="Nesta",
+    license="MIT",
+)
